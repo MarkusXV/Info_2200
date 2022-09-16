9dd82e1ecd3cbff4e24836ccf67e4fefccb5d5f8
MarkusXV <10934984@uvu.edu>
2022-09-15 21:04:19 -0600

    Comments for half of M02Part2

--- M02/M02Part2/Phone.cs
+++ M02/M02Part2/Phone.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace M02Part2
 {
-    public class Phone
+    public class Phone //Creates the phone class where we create the array definitions of the phone details
     {
-        public Phone(string[] phone)
+        public Phone(string[] phone) //Method that takes the list of the phone details from the text file and splits them into the seperate details
         {
             Make = phone[0];
             Model = phone[1];
@@ -17,6 +17,9 @@ namespace M02Part2
             Price = decimal.Parse(phone[4]);
         }
 
+        /// <summary>
+        /// Lets us have different Phone Details for each phone we pass into the method and sets it to those same values
+        /// </summary>
         public string Make { get; set; }
         public string Model { get; set; }
         public string Display { get; set; }
