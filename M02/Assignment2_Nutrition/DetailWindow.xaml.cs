89c3ce4af2bbbd0a75b380a5b41abeaf39488a1f
MarkusXV <10934984@uvu.edu>
2022-09-16 21:21:03 -0600

    Finished the Project, not the sudo code

--- M02/Assignment2_Nutrition/DetailWindow.xaml.cs
+++ M02/Assignment2_Nutrition/DetailWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace Assignment2_Nutrition
@@ -22,6 +23,38 @@ namespace Assignment2_Nutrition
         public DetailWindow()
         {
             InitializeComponent();
+        }
+
+        public void UpdateGUI(Food selectedFood)
+        {
+            LblFood.Content =         selectedFood.Foodkey;
+            LblCalcium.Content =      selectedFood.Calcium;
+            LblServingSize.Content =  selectedFood.ServingSize;
+            LblWeight.Content =       selectedFood.Weight;
+            LblPercentWater.Content = selectedFood.PercentWater;
+            LblCalories.Content =     selectedFood.Energy;
+            LblProtein.Content =      selectedFood.Protein;
+            LblCarbohydrate.Content = selectedFood.Carbohydrate;
+            LblFiber.Content =        selectedFood.Fiber;
+            LblCholesterol.Content =  selectedFood.Cholesterol;
+            LblTotalFat.Content =     selectedFood.TotalFat;
+            LblSaturateFat.Content =  selectedFood.SaturateFat;
+            LblMonosatFat.Content =   selectedFood.MonosatFat;
+            LblPolyunsatFat.Content = selectedFood.PolyunsatFat;
+            LblIron.Content =         selectedFood.Iron;
+            LblMagnesium.Content =    selectedFood.Magnesium;
+            LblSodium.Content =       selectedFood.Sodium;
+            LblPhosphorous.Content =  selectedFood.Phosphorous;
+            LblCaP.Content =          selectedFood.CaP;
+            LblPotassium.Content =    selectedFood.Potassium;
+            LblZinc.Content =         selectedFood.Zinc;
+            LblNiacin.Content =       selectedFood.Niacin;
+            LblThiamin.Content =      selectedFood.Thiamin;
+            LblRiboflavin.Content =   selectedFood.Riboflavin;
+            LblVitA.Content =         selectedFood.VitA;
+            LblVitC.Content =         selectedFood.VitC;
+            LblVitB6.Content =        selectedFood.VitB6;
+            LblFoodType.Content =     selectedFood.FoodType;
         }
     }
 }
--- M02/Assignment2_Nutrition/Food.cs
+++ M02/Assignment2_Nutrition/Food.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace Assignment2_Nutrition
 {
-    internal class Food
+    public class Food
     {
+        public Food(string[] food)
+        {
+            Foodkey =       food[0];
+            Calcium =       double.Parse(food[1]);
+            ServingSize =   food[2];
+            Weight =        double.Parse(food[3]);
+            PercentWater =  double.Parse(food[4]);
+            Energy =        double.Parse(food[5]);
+            Protein =       double.Parse(food[6]);
+            Carbohydrate =  double.Parse(food[7]);
+            Fiber =         double.Parse(food[8]);
+            Cholesterol =   double.Parse(food[9]);
+            TotalFat =      double.Parse(food[10]);
+            SaturateFat =   double.Parse(food[11]);
+            MonosatFat =    double.Parse(food[12]);
+            PolyunsatFat =  double.Parse(food[13]);
+            Iron =          double.Parse(food[14]);
+            Magnesium =     double.Parse(food[15]);
+            Sodium =        double.Parse(food[16]);
+            Phosphorous =   double.Parse(food[17]);
+            CaP =           double.Parse(food[18]);
+            Potassium =     double.Parse(food[19]);
+            Zinc =          double.Parse(food[20]);
+            Niacin =        double.Parse(food[21]);
+            Thiamin =       double.Parse(food[22]);
+            Riboflavin =    double.Parse(food[23]);
+            VitA =          double.Parse(food[24]);
+            VitC =          double.Parse(food[25]);
+            VitB6 =         double.Parse(food[26]);
+            FoodType =      food[27];
+
+        }
+
+        public string Foodkey       { get; set; }
+        public double Calcium       { get; set; }
+        public string ServingSize   { get; set; }
+        public double Weight        { get; set; }
+        public double PercentWater  { get; set; }
+        public double Energy        { get; set; }
+        public double Protein       { get; set; }
+        public double Carbohydrate  { get; set; }
+        public double Fiber         { get; set; }
+        public double Cholesterol   { get; set; }
+        public double TotalFat      { get; set; }
+        public double SaturateFat   { get; set; }
+        public double MonosatFat    { get; set; }
+        public double PolyunsatFat  { get; set; }
+        public double Iron          { get; set; }
+        public double Magnesium     { get; set; }
+        public double Sodium        { get; set; }
+        public double Phosphorous   { get; set; }
+        public double CaP           { get; set; }
+        public double Potassium     { get; set; }
+        public double Zinc          { get; set; }
+        public double Niacin        { get; set; }
+        public double Thiamin       { get; set; }
+        public double Riboflavin    { get; set; }
+        public double VitA          { get; set; }
+        public double VitC          { get; set; }
+        public double VitB6         { get; set; }
+        public string FoodType      { get; set; }
+
+
+
     }
 }
